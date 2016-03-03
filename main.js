23e5fcad623b40d48a29e41b679e59c94f9b6820
Calin Seciu <calinseciu@gmail.com>
2016-03-03 21:34:58 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+require('./scss/app.scss');
+var Pagedown = require("exports?Pagedown!./js/pagedown");
+
+console.log(Pagedown);
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var path = require('path');
+var webpack = require('webpack');
+var bourbon = require('node-bourbon').includePaths;
+
+var commonsPlugin =
+    new webpack.optimize.CommonsChunkPlugin('common.js');
+
+module.exports = {
+    entry: [
+        './main.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'public'),
+        publicPath: '/', // This is used to generate URLs to e.g. images
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'/*, 'react'*/]
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css?sourceMap'
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css!resolve-url!sass?sourceMap'
+            },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest
+            {
+                test: /\.(woff|woff2)$/,
+                loader: "url?limit=5000"
+            }
+        ]
+    },
+    sassLoader: {
+        includePaths: [bourbon]
+    },
+    // resolveUrlLoader: {
+    //     root: path.dirname(require.resolve('classets/package'))
+    // },
+    resolve: {
+        // you can now require('file') instead of require('file.coffee')
+        extensions: ['', '.js', '.json']
+    },
+    devServer: {
+        contentBase: "./public",
+        noInfo: true, //  --no-info option
+        hot: true,
+        inline: true
+    },
+    plugins: [
+        commonsPlugin
+    ]
+};

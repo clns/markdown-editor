f12b723c038adc937996b66d042b2e06a00d3a26
Calin Seciu <calinseciu@gmail.com>
2016-03-17 23:32:06 +0200

    More stuff into scope

--- src/editor/scope.js
+++ src/editor/scope.js
@@ -1,27 +1,57 @@
 // http://teropa.info/blog/2013/11/03/make-your-own-angular-part-1-scopes-and-digest.html
-export default function Scope() {
+
+export default var rootScope = new Scope();
+
+export function Scope() {
     this.$$watchers = [];
+    this.$$asyncQueue = [];
+    this.$$phase = null;
 }
 
 Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
+    var self = this;
+    if (typeof watchFn == 'string') {
+        var s = watchFn;
+        watchFn = function(scope) {
+            var parts = s.split('.');
+            var v = scope;
+            do {
+                v = v[parts.shift()];
+            } while(typeof v != 'undefined' && parts.length);
+            return v;
+        }
+    }
     var watcher = {
         watchFn: watchFn,
         listenerFn: listenerFn || function() { },
         valueEq: !!valueEq
     };
-    this.$$watchers.push(watcher);
+    self.$$watchers.push(watcher);
+    return function() {
+        var index = self.$$watchers.indexOf(watcher);
+        if (index >= 0) {
+            self.$$watchers.splice(index, 1);
+        }
+    };
 };
 
 Scope.prototype.$$digestOnce = function() {
     var self = this;
     var dirty;
     this.$$watchers.forEach(function(watch) {
-        var newValue = watch.watchFn(self);
-        var oldValue = watch.last;
-        if (newValue !== oldValue) {
-            watch.listenerFn(newValue, oldValue, self);
-            dirty = true;
-            watch.last = newValue;
+        try {
+            var newValue = watch.watchFn(self);
+            var oldValue = watch.last;
+            var isEqual = newValue === oldValue ||
+                (typeof newValue === 'number' && typeof oldValue === 'number' &&
+                isNaN(newValue) && isNaN(oldValue));
+            if (!isEqual) {
+                watch.listenerFn(newValue, oldValue, self);
+                dirty = true;
+                watch.last = newValue;
+            }
+        } catch (e) {
+            (console.error || console.log)(e);
         }
     });
     return dirty;
@@ -30,10 +60,59 @@ Scope.prototype.$$digestOnce = function() {
 Scope.prototype.$digest = function() {
     var ttl = maxTTL = 10;
     var dirty;
+    this.$beginPhase("$digest");
     do {
+        while (this.$$asyncQueue.length) {
+            try {
+                var asyncTask = this.$$asyncQueue.shift();
+                this.$eval(asyncTask.expression);
+            } catch (e) {
+                (console.error || console.log)(e);
+            }
+        }
         dirty = this.$$digestOnce();
         if (dirty && !(ttl--)) {
+            this.$clearPhase();
             throw maxTTL + " digest iterations reached";
         }
     } while (dirty);
+    this.$clearPhase();
+};
+
+
+Scope.prototype.$eval = function(expr, locals) {
+    return expr(this, locals);
+};
+
+Scope.prototype.$evalAsync = function(expr) {
+    var self = this;
+    if (!self.$$phase && !self.$$asyncQueue.length) {
+        setTimeout(function() {
+            if (self.$$asyncQueue.length) {
+                self.$digest();
+            }
+        }, 0);
+    }
+    this.$$asyncQueue.push({scope: this, expression: expr});
+};
+
+Scope.prototype.$apply = function(expr) {
+    try {
+        this.$beginPhase("$apply");
+        return this.$eval(expr);
+    } finally {
+        this.$clearPhase();
+        this.$digest();
+    }
+};
+
+Scope.prototype.$beginPhase = function(phase) {
+    if (this.$$phase) {
+        throw this.$$phase + ' already in progress.';
+    }
+    this.$$phase = phase;
+};
+
+Scope.prototype.$clearPhase = function() {
+    this.$$phase = null;
 };
